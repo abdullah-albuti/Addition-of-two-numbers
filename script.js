ab9072394a6e5a841c3cf030fd3f4cfc31f71bb8
Abdullah Albuti <43214508+abdullah-albuti@users.noreply.github.com>
2024-12-25 01:58:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+run=()=>{
+
+    var  num1 = Number(document.getElementById('number1').value);
+
+    var num2 = Number(document.getElementById('number2').value);
+    var Sumd  = document.getElementById('sum');
+    
+    var s = num1+num2;
+    Sumd.innerHTML = s;
+    
+  }
